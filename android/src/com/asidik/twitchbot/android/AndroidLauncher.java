57872697ddc8048db3a8a4421cc8197f3b909fc7
TomSki <tomaszwojciechowski@hotmail.co.uk>
2015-02-03 20:53:34 +0000

    meow

---
+++ android/src/com/asidik/twitchbot/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.asidik.twitchbot.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.asidik.twitchbot.Bot;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Bot(), config);
+	}
+}
---
+++ desktop/src/com/asidik/twitchbot/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.asidik.twitchbot.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.asidik.twitchbot.Bot;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Bot(), config);
+	}
+}
