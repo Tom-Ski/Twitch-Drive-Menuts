10ff3db3affbb0708fbcd9e65f9b6660d3e5b6db
TomSki <tomaszwojciechowski@hotmail.co.uk>
2015-02-06 22:42:19 +0000

    Icons

--- core/src/com/asidik/twitchbot/widgets/InputWindow.java
+++ core/src/com/asidik/twitchbot/widgets/InputWindow.java
@@ -1,7 +1,11 @@
 package com.asidik.twitchbot.widgets;
 
+import com.asidik.twitchbot.InputType;
 import com.asidik.twitchbot.UserInput;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.scenes.scene2d.ui.*;
 import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
@@ -13,16 +17,38 @@ public class InputWindow extends Window {
     Array<UserInput> inputBuffer = new Array<UserInput>();
     Array<UserInput> immaStealYoInputs = new Array<UserInput>();
 
+    Image left;
+    Image right;
+    Image up;
+    Image down;
+
     int maxBufferSize = 10;
     Skin skin;
     int count = 0;
 
-    public InputWindow (String title, WindowStyle style, Skin skin) {
+    public InputWindow(String title, WindowStyle style, Skin skin) {
         super(title, style);
         this.skin = skin;
         refresh();
         top();
-        defaults().fillX().expandX().height(30);
+        defaults().fillX().expandX().height(30).pad(0, 20, 0, 20);
+
+        Sprite arrowSprite = new Sprite(new Texture(Gdx.files.internal("Right.png")));
+        arrowSprite.setOrigin(arrowSprite.getWidth()/2f, arrowSprite.getHeight()/2f);
+
+        left = new Image(arrowSprite);
+        right = new Image(arrowSprite);
+        up = new Image(arrowSprite);
+        down = new Image(arrowSprite);
+
+        left.setOrigin(arrowSprite.getWidth()/2f, arrowSprite.getHeight()/2f);
+        right.setOrigin(arrowSprite.getWidth()/2f, arrowSprite.getHeight()/2f);
+        up.setOrigin(arrowSprite.getWidth() / 2f, arrowSprite.getHeight() / 2f);
+        down.setOrigin(arrowSprite.getWidth() / 2f, arrowSprite.getHeight() / 2f);
+
+        left.setRotation(180f);
+        up.setRotation(90f);
+        down.setRotation(-90f);
     }
 
     @Override
@@ -50,7 +76,7 @@ public class InputWindow extends Window {
         super.draw(batch, parentAlpha);
     }
 
-    public void addVote (UserInput userInput) {
+    public void addVote(UserInput userInput) {
         if (inputBuffer.size > maxBufferSize) {
             inputBuffer.removeValue(inputBuffer.first(), false);
         }
@@ -62,11 +88,28 @@ public class InputWindow extends Window {
         getChildren().clear();
         getCells().clear();
         for (UserInput message : inputBuffer) {
+            Table labelContainer = new Table();
             Label label = new Label(message.userName, skin);
-            add(label);
+            labelContainer.add(label).expandX();
+            labelContainer.add(getArrow(message.input));
+            add(labelContainer);
             row();
         }
+    }
 
+
+    private Image getArrow (InputType type) {
+        switch (type) {
+            case UP:
+                return up;
+            case DOWN:
+                return down;
+            case RIGHT:
+                return right;
+            case LEFT:
+                return left;
+        }
+        return null;
     }
 
 
